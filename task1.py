ece4f1f58169fa21b641c5f247599b36601c76a1
alyssa-higdon <alyssahigdon@Alyssas-MacBook-Pro.local>
2023-04-18 11:07:18 -0700

    lab time Tuesday

--- task1.py
+++ task1.py
@@ -1,16 +1,36 @@
 # csc 321
 # lab 2
 # task 1
-from Cryptodome.Cipher import AES
-# aes_obj = AES.new(key, AES.MODE_ECB)
-# aes_obj.encrypt(bytes(padded_plain), "UTF-8")
+#from Cryptodome.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+
+
+
+
+BLOCKSIZE = AES.block_size
 
 def ecb_mode(plain):
+   
+    key = get_random_bytes(BLOCKSIZE)
+    iv = get_random_bytes(BLOCKSIZE)
+    
+    #read plain text as binary
+    with open(plain, "rb") as f:
+        padded = pad(f , AES.block_size, style='pkcs7')
+        aes_obj = AES.new(key, AES.MODE_ECB)
+        cipher_text = aes_obj.encrypt(bytes(padded), "UTF-8")
+        # create an AES Object to encrypt in ECB mode
+        #write out E to append to a file here?
+      
+        
+    
     # ecb
-    # generate a random key
     # write encyption of plaintext in a new file
     outfile = open("out.txt", "w")
 
 
 def cbc_mode(plain):
     # cbc
+    pass
