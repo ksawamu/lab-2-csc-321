3e46dab9fb0c2a7bb6ca10872cdbfcc07bc2140f
Kara Sawamura <sawamurakara@gmail.com>
2023-04-18 10:25:06 -0700

    Set up task 1 file

--- task1.py
+++ task1.py
@@ -1,3 +1,16 @@
 # csc 321
 # lab 2
 # task 1
+from Cryptodome.Cipher import AES
+# aes_obj = AES.new(key, AES.MODE_ECB)
+# aes_obj.encrypt(bytes(padded_plain), "UTF-8")
+
+def ecb_mode(plain):
+    # ecb
+    # generate a random key
+    # write encyption of plaintext in a new file
+    outfile = open("out.txt", "w")
+
+
+def cbc_mode(plain):
+    # cbc
